4e64efb60133c0f91651b82eaac9221a7000bd33
ivy.ho <ivy.ho@udngroup.com>
2023-07-24 21:47:12 +0800

    add: slideIn motion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,11 @@
 import { motion } from 'framer-motion';
-import { Link } from "react-router-dom";
 
 import { styles } from '../styles';
 import { fadeIn, textVariant  } from '../utils/motion';
 import { SectionWrapper } from '../hoc';
 import Tech from './Tech';
 import Button from './Button';
+import { slideIn } from '../utils/motion'
 
  
 const About = () => {
@@ -25,8 +25,8 @@ const About = () => {
           className='mt-4 text-secondary text-[17px] max-w-2xl leading-[30px] mb-10 flex flex-col'
         >
           <p className='tracking-wide mb-8'>
-            <span className='block text-violet-500 text-xl font-medium mb-4'>「真的好想做出那個酷東西！」</span>
-            基於對動態網頁製作的熱愛，開始摸索相關技術，希望做出與使用者產生更多互動，也更富趣味性的網站。更期許自己能善用 React, Vue 等技術，製作出更多富有人性化的介面、為使用者帶來便利的網站、應用程式和系統。
+            <span className='block text-violet-500 text-xl font-medium mb-4'>「好想做出那個酷東西！」</span>
+            基於對動態網頁製作的熱愛，開始摸索相關技術，希望做出與使用者產生互動，富趣味性的網站。也期許自己能善用 React, Vue 等技術，製作出更多富有人性化的介面、為使用者帶來便利的網站、應用程式和系統。
           </p>
           <Button
             path={'/portfolio/about'}
@@ -36,9 +36,11 @@ const About = () => {
         </motion.div>
       </div>
 
-      <div>
+      <motion.div
+        variants={slideIn('left', "tween", 0.2, 1)}
+      >
         <Tech />
-      </div>
+      </motion.div>
     </div>
   )
 }
