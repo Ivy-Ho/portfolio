e329acdbe9c4aec5adeee58f20a9c0e05c1dc2c7
ivy.ho <ivy.ho@udngroup.com>
2023-07-24 22:05:52 +0800

    add: text content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
           className='mt-4 text-secondary text-[17px] max-w-2xl leading-[30px] mb-10 flex flex-col'
         >
           <p className='tracking-wide mb-8'>
-            <span className='block text-violet-500 text-xl font-medium mb-4'>「好想做出那個酷東西！」</span>
+            <span className='block text-violet-500 text-xl font-medium mb-4'>「真的好想做出那個酷東西！」</span>
             基於對動態網頁製作的熱愛，開始摸索相關技術，希望做出與使用者產生互動，富趣味性的網站。也期許自己能善用 React, Vue 等技術，製作出更多富有人性化的介面、為使用者帶來便利的網站、應用程式和系統。
           </p>
           <Button
