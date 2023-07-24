0fbcc55aacb0984d88dc28c3398059925a477bf0
ivy.ho <ivy.ho@udngroup.com>
2023-07-24 10:50:12 +0800

    add: loading screen

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import ScrollToTop from "../helpers/ScrollToTop";
 import { Outlet } from 'react-router-dom'
-import { Footer, Navbar, GoTop } from '../components';
+import { Footer, Navbar, GoTop, LoadingScreen } from '../components';
+
 
 export default function RootLayout() {
+
+  const [started, setStarted] = useState(false);
+
  return (
   <div className="root-layout">
+    <LoadingScreen started={started} setStarted={setStarted} />  
     <Navbar />
     <ScrollToTop />
     <main>
