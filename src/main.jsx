fb7e498157d9b71dc3e78bef26746fd176a14c49
ivy.ho <ivy.ho@udngroup.com>
2023-07-30 22:54:11 +0800

    remove: comma

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <ScreenProvider>
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
+    </React.StrictMode>
   </ScreenProvider>
 )
